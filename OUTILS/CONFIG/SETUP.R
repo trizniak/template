3b6e11a114c033bda0c3b3c5e329dd9feaf3809e
Bogdan Micu <38376081+trizniak@users.noreply.github.com>
2020-05-09 15:19:18 +0200

    Update SETUP.R

--- OUTILS/CONFIG/SETUP.R
+++ OUTILS/CONFIG/SETUP.R
@@ -2,10 +2,11 @@
 
 # Code appearance ([Tools] [Global Options] [Appearance]) : Tomorrow Night Blue
 
-# FOLDERS
-# folders=c("DATA","OUTILS","OUTPUT","T E M P","WHOREHOUSE");for (i in 1:length(folders)) {dir.create(paste("../",folders[i], sep="/"))}
-
-options(repr.plot.width=49,repr.plot.height=36,scipen=999,digits=1,warn=-1)
+options(repr.plot.width=49,
+				repr.plot.height=36,
+				scipen=999,
+				digits=1,
+				warn=-1)
 my.color="#273749" # 214263 0B4279 0b2131 1b3142
 
 
@@ -50,15 +51,19 @@ pacman::p_load_gh("wilkelab/ggtext",
 # ---- THEME ----
 my.theme = function() {
   theme_minimal() +
-    theme(text=element_text(family="Calibri",color=my.color),
-          axis.line.x.bottom=element_line(color="grey",size=.3), # set as element_blank to remove : axis.line is ignored
-          axis.line.y.left=element_line(color="grey",size=.3), # set as element_blank to remove : axis.line is ignored
+    theme(text=element_text(family="Calibri",
+														color=my.color),
+          axis.line.x.bottom=element_line(color="grey",
+																					size=.3),	# set as element_blank to remove : axis.line is ignored
+          axis.line.y.left=element_line(color="grey",
+																				size=.3),		# set as element_blank to remove : axis.line is ignored
           axis.text=element_blank(),
           axis.ticks=element_blank(),
           axis.title=element_text(face="italic"),
           legend.title=element_blank(),
           panel.background=element_blank(),
-          panel.border=element_rect(size=0.1,fill=NA),
+          panel.border=element_rect(size=0.1,
+																		fill=NA),
           panel.grid=element_blank(),
           panel.spacing=unit(0.1,"lines"),
           plot.title=element_markdown(),
@@ -66,18 +71,49 @@ my.theme = function() {
           plot.subtitle=element_markdown(),
           strip.background=element_blank(),
           strip.placement="outside",
-          strip.text=element_text(color=my.color,face="italic"))
+          strip.text=element_text(color=my.color,
+																	face="italic"))
 }
 
 theme_set(my.theme())
 
 
 # ---- Color Palette : Age groups ----
-palette.AgeG = c("royalblue4","brown4","darkorange","gold","tan","khaki4","darkgreen")#;scales::show_col(palette.AgeG)
-names(palette.AgeG) = c("ALL","<18","18-24","25-34","35-54","55-64","65+")
+# 6 groups
+palette.AgeG6=c("royalblue4",
+							 "brown4",
+							 "darkorange",
+							 "gold",
+							 "tan",
+							 "khaki4",
+							 "darkgreen")
+#	scales::show_col(palette.AgeG6)
+names(palette.AgeG6)=c("ALL",
+											"<18",
+											"18-24",
+											"25-34",
+											"35-54",
+											"55-64",
+											"65+")
+# 4 groups
+palette.AgeG4=c("royalblue4",
+							 "brown4",
+							 "gold",
+							 "khaki4",
+							 "darkgreen")
+#	scales::show_col(palette.AgeG4)
+names(palette.AgeG4)=c("ALL",
+											"<18",
+											"18-34",
+											"35-64",
+											"65+")
 
 # ---- Color Palette : Clusters ----
-palette.clu = c("firebrick","mediumorchid4","forestgreen","darkblue")#;scales::show_col(palette.clu)
+palette.clu = c("firebrick",
+								"mediumorchid4",
+								"forestgreen",
+								"darkblue")
+#	scales::show_col(palette.clu)
 
 
 # ---- FUNCTION : Label Color ----
@@ -90,9 +126,9 @@ f.label.color = function(x,color.neg="green3",color.poz="red") {
          "'>",x,"</span>")}
 
 # ---- EU MS : Protocol Order ----
-# UPLOAD to OUTILS/REF : \\net1.cec.eu.int\ESTAT\F\1\common\03 flash estimates\BOGDAN\EU.PO.txt
-EU.PO = read_delim(here("OUTILS","REF","EU.PO.txt"),
-                   "\t",escape_double=FALSE,trim_ws=TRUE) %>%
+EU.PO = read_delim(here("OUTILS","REF","EU.PO.txt"),"\t",
+									 escape_double=FALSE,
+									 trim_ws=TRUE) %>%
   as.data.frame() %>%
   filter(COUNTRY!="UK")
 
