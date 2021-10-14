b99886f1f4a509782ef5ec3c42558f22df7fbf7c
Bogdan Micu <38376081+trizniak@users.noreply.github.com>
2021-10-14 11:49:49 +0200

    Update _OUTPUT.R

--- R/_OUTPUT.R
+++ R/_OUTPUT.R
@@ -1,82 +1,43 @@
 
-# ==== OUTPUT for UNIDEMO Quality Report ====
-# #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
+# ==== OUTPUT for XXXXXXX ====
+# #### ~~~~~~~~~~~~~~~~~~ ####
 
-# * DataInfo ####
-load("./data/DataInfo.RData")
-# ... recreate DataInfo TOC.txt from saved R object ####
-# otherwise, at every run of W.DOC.R without _DATA.R same entries are multiplied (_DATA.R initializes DataInfo TOC.txt)
-cat(file.TOC,
-    file=.file.TOC,
-    sep="")
+
+# ~~~ INIT ~~~ ####
+
+# * DATA ####
+# load("./data/data.XXXXXXX.RData")
 
 # * FUNS ####
-source("./R/F.VIZ.R")
+# source("./R/F.VIZ.R")
+
+# * Sections to run ####
+viz.XXXXXXX=FALSE
+tab.XXXXXXX=FALSE
+doc.XXXXXXX=FALSE
+report.XXXXXXX=FALSE
 
 
-# [ Sections to run ] ####
-viz.CountryCounts=TRUE
-viz.DeliveryCalendar=FALSE
-doc.Tables=FALSE
+# ~~~ OUTPUT SECTIONS ~~~ ####
 
-
-# ~~~ VIZ : Country counts ~~~ ####
-# * output ----
-# ... delete old output ####
-file.remove(Sys.glob("./output/viz/*Country Counts*.png"))
-# ... output ####
-if (viz.CountryCounts) {
-  pmap(expand.grid(.table_group=seq(length(unique(UNIDEMO.tables$Table.group))),
-                   detailed.country=FALSE,
-                   save=TRUE),
-       F.viz.CountryCounts)
-  F.viz.CountryCounts(save=TRUE)
-}
-
-
-# ~~~ VIZ : Delivery Calendar ~~~ ####
-# * output ----
-# ... delete old output ####
-file.remove(Sys.glob("./output/viz/*Delivery Calendar*.png"))
-# ... output ####
-if (viz.DeliveryCalendar) {
-  map(1:3,
-      F.viz.DeliveryCalendar,
-      .datafile=data.UNIDEMO.delivery,
-      save=TRUE)
-}
-
-
-# ~~~ DOC : Tables ~~~ ####
-# ... delete old output ####
-file.remove("./output/doc/UNIDEMO QR [TAB].docx")
-# ... output ####
-if (doc.Tables) {
-  source("./R/W.DOC.R")
-}
+# XXXXXXX ~~~ ####
+# XXXXXXX ####
+# * XXXXXXX ####
+# ... XXXXXXX ####
 
 
 # ~~~ COLLECT ~~~ ####
 
-# DataInfo ####
-# ... add TOC ####
-DataInfo %>%
-  openxlsx::writeData(sheet="CONTENTS",
-                      x=readLines(.file.TOC))
-# ... save to excel ####
-openxlsx::saveWorkbook(DataInfo,
-                       "./data/UNIDEMO QR - Data Info.xlsx",
-                       overwrite=TRUE)
-
-# * Collect output & data info ####
-.archive.output="./output/UNIDEMO QR.zip"
-#file.remove(.archive.output)
+# ... zip ####
+.archive.output="./output/XXXXXXX.zip"
 zip::zip(.archive.output,
-    files=c(Sys.glob("./data/*UNIDEMO QR*.*"),
-            Sys.glob("./output/viz/*UNIDEMO QR*.*g"),
-            Sys.glob("./output/doc/*UNIDEMO QR*.doc*")),
+    files=c(Sys.glob("./data/*XXXXXXX*.*"),
+            Sys.glob("./output/*XXXXXXX*.htm*"),
+            Sys.glob("./output/viz/*XXXXXXX*.*g"),
+            Sys.glob("./output/doc/*XXXXXXX*.doc*")),
     include_directories=FALSE,
     mode="cherry-pick")
+
 # ... message ####
 message(gsub("/","\\",
              paste0(getwd(),
