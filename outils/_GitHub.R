8c6bdef192635ff53cd864e5f110ba4422d8cadf
Bogdan Micu <38376081+trizniak@users.noreply.github.com>
2021-10-14 13:40:33 +0200

    Update _GitHub.R

--- outils/_GitHub.R
+++ outils/_GitHub.R
@@ -1,10 +1,12 @@
 
-# 3. Configure git with Rstudio ##############################################
-# SOURCE : https://gist.github.com/Z3tt/3dab3535007acf108391649766409421
+# ==== Configure git with Rstudio ====
+# #### ~~~~~~~~~~~~~~~~~~~~~~~~~~ ####
 
-## set your user name and email :
+# set your user name and email :
 usethis::use_git_config(user.name="YourName",
                         user.email="your@mail.com")
 
-## set personal access token :
+# set personal access token :
 credentials::set_github_pat("YourPAT")
+
+# SOURCE : https://gist.github.com/Z3tt/3dab3535007acf108391649766409421
