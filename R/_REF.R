8987a709a536d73ba5899264e651184ff7585847
Bogdan Micu <38376081+trizniak@users.noreply.github.com>
2021-10-14 12:02:22 +0200

    Update _REF.R

--- R/_REF.R
+++ R/_REF.R
@@ -66,68 +66,11 @@ COUNTRY.LIST = jsonlite::fromJSON(url("http://ec.europa.eu/eurostat/wdds/rest/da
   dplyr::arrange(PO,COUNTRY) %>%
   dplyr::mutate(PO=as.numeric(tidyr::replace_na(rownames(.))))
 
+
+# [ SAVE Ref ] ####
+
 save("EU.MS",
      "EFTA.MS",
      "EU.Candidates",
      "COUNTRY.LIST",
      file="./outils/countries.RData")
-
-
-# ~~~ UNIDEMO Tables ~~~ ####
-# ... tables ####
-UNIDEMO.tables = readr::read_delim("./outils/UNIDEMO_tables.txt") %>%
-  dplyr::mutate(Table.family=stringr::str_remove_all(Table,
-                                                     "[[:digit:]]"),
-                Table.family=dplyr::recode(Table.family,
-                                           `E/E`="E",
-                                           `I/I`="I",
-                                           `A/A`="A"),
-                Table.group=factor(
-                  dplyr::case_when(Table.family=="P" ~
-                                     "Population",
-                                   Table.family %in% c("B","L") ~
-                                     "Births",
-                                   Table.family %in% c("D","X") ~
-                                     "Deaths",
-                                   Table.family %in% c("M","S") ~
-                                     "Marriages &\nDivorces",
-                                   Table.family %in% c("E","I","A","LC") ~
-                                     "Migration &\nCitizenship"),
-                  levels=c("Population",
-                           "Births",
-                           "Deaths",
-                           "Marriages &\nDivorces",
-                           "Migration &\nCitizenship")),
-                MANDATORY=as.numeric(stringr::str_detect(tolower(Regulations),
-                                                         "mandatory")))
-UNIDEMO.Q.tables = UNIDEMO.tables %>%
-  dplyr::group_by(Questionnaire,
-                  Table.family) %>%
-  dplyr::summarize() %>%
-  dplyr::relocate(Table.family) %>%
-  tibble::deframe()
-
-# * Composite tables ####
-UNIDEMO.composite.tables = utils::read.table(header=FALSE,
-                                             sep=";",
-                                             col.names=c("order",
-                                                         "tab.1",
-                                                         "tab.2"),
-                                             text=
-"1;I01;I06
-2;I02;I07
-3;I03;I08
-4;I04;I09
-5;I05;I10
-6;E01;E05
-7;E02;E06
-8;E03;E07
-9;E04;E08
-10;A00;A01")
-
-# [ SAVE Ref ] ####
-
-save("UNIDEMO.tables",
-     "UNIDEMO.Q.tables",
-     "UNIDEMO.composite.tables",
-     file="./outils/tables.RData")
